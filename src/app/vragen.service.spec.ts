2d3a686b02ba4f4444090b4eabb39469456103d2
Nick <nick.braks@live.nl>
2019-12-06 15:17:53 +0100

    Merge branch 'development' of ssh://94.124.143.192:7999/xcltdg/qoding-challenge-beheersomgeving into feature/XCLTDG-149-front-end-implementatie
    # Conflicts:
    #	src/app/app-routing.module.ts
    #	src/app/app.module.ts

---
+++ src/app/vragen.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { VragenService } from './vragen.service';
+
+describe('VragenService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: VragenService = TestBed.get(VragenService);
+    expect(service).toBeTruthy();
+  });
+});
