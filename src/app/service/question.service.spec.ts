d6f54c0eaafc804a18cd39119c23c8229e1ee35c
Mark <contactmarkjansen@gmail.com>
2019-12-18 15:52:00 +0100

    added max slider

--- src/app/service/question.service.spec.ts
+++ src/app/service/question.service.spec.ts
@@ -1,16 +1,19 @@
-import { TestBed, async } from '@angular/core/testing';
-import { QuestionService } from '../service/question.service';
-import { HttpClientModule } from '@angular/common/http';
-import { ToastrModule, ToastrService } from 'ngx-toastr';
-import { Question } from '../class/question';
-import { Observable } from 'rxjs';
-import { GivenAnswer } from '../class/given-answer';
+import {async, TestBed} from '@angular/core/testing';
+import {QuestionService} from '../service/question.service';
+import {HttpClientModule} from '@angular/common/http';
+import {ToastrModule, ToastrService} from 'ngx-toastr';
+import {Question} from '../class/question';
+import {Observable} from 'rxjs';
+import {GivenAnswer} from '../class/given-answer';
+import {RouterTestingModule} from '@angular/router/testing';
+
 describe('Question Service', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         HttpClientModule,
-        ToastrModule.forRoot()
+        ToastrModule.forRoot(),
+        RouterTestingModule
       ],
       providers: [
         QuestionService,
