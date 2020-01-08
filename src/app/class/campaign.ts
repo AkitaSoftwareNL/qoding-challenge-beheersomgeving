0ea0fb824084b29208078605f3c6b0b3581780fe
Bas-Martin <48922519+Bas-Martin@users.noreply.github.com>
2020-01-08 15:33:39 +0100

    added

--- src/app/class/campaign.ts
+++ src/app/class/campaign.ts
@@ -1,9 +1,16 @@
+import { AmountOfQuestionTypeCollection } from './amountOfQuestionTypeCollection';
+
 export class Campaign {
   id: number;
   name: string;
-  amountOfQuestions: bigint;
+  amountOfQuestions: AmountOfQuestionTypeCollection;
   startedBy: string;
   category: string;
   participants: string;
   date: number;
+
+  constructor(name: string, amountOfQuestions: AmountOfQuestionTypeCollection) {
+    this.name = name;
+    this.amountOfQuestions = amountOfQuestions;
+  }
 }
---
+++ src/app/class/campaignDTO.ts
@@ -0,0 +1,10 @@
+
+export class campaignDTO {
+    constructor(
+        public name: string,
+        public amountTotal: number,
+        public amountOpen: number,
+        public amountMultiple: number,
+        public amountProgram: number,
+    ) { }
+}
