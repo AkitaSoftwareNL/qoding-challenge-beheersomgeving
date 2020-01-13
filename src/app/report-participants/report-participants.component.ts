4e842c04a6c55ff0a11cc8558bf35123a2aa307d
BeingBright <BrightonvandenEnd@gmail.com>
2020-01-13 11:10:47 +0100

    Time Display Update

--- src/app/report-participants/report-participants.component.ts
+++ src/app/report-participants/report-participants.component.ts
@@ -1,12 +1,12 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTable} from '@angular/material/table';
-import {ReportParticipantsDatasource} from './report-participants-datasource';
-import {CampaignService} from '../service/campaign.service';
-import {Participant} from '../class/participant';
-import {ActivatedRoute} from '@angular/router';
-import {Subscription} from 'rxjs';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { ReportParticipantsDatasource } from './report-participants-datasource';
+import { CampaignService } from '../service/campaign.service';
+import { Participant } from '../class/participant';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-rapport-participants',
@@ -14,9 +14,9 @@ import {Subscription} from 'rxjs';
   styleUrls: ['./report-participants.component.css']
 })
 export class ReportParticipantsComponent implements AfterViewInit, OnInit {
-  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: false}) sort: MatSort;
-  @ViewChild(MatTable, {static: false}) table: MatTable<Participant>;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  @ViewChild(MatTable, { static: false }) table: MatTable<Participant>;
   dataSource: ReportParticipantsDatasource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
@@ -48,5 +48,23 @@ export class ReportParticipantsComponent implements AfterViewInit, OnInit {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
     this.table.dataSource = this.dataSource;
+  }
+
+  convertTime(timeInMiliseconds: number) {
+    let h: number;
+    let m: number;
+    let s: number;
+    h = Math.floor(timeInMiliseconds / 1000 / 60 / 60);
+    m = Math.floor((timeInMiliseconds / 1000 / 60 / 60 - h) * 60);
+    s = Math.floor(((timeInMiliseconds / 1000 / 60 / 60 - h) * 60 - m) * 60);
+
+    let sec: string = s < 10 ? `0${s}` : `${s}`;
+    let min: string = m < 10 ? `0${m}` : `${m}`;
+    let hour: string = h < 10 ? `0${h}` : `${h}`;
+
+
+    console.log();
+
+    return `${hour}:${min}:${sec}`;
   }
 }
