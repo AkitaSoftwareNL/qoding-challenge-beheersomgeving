bcd235f9111bb424e6fdeb2b161d422d43149257
Mark <contactmarkjansen@gmail.com>
2020-01-13 11:26:30 +0100

    Meerkeuze

--- src/app/report-participant-question/report-participant-question.component.ts
+++ src/app/report-participant-question/report-participant-question.component.ts
@@ -55,7 +55,7 @@ export class ReportParticipantQuestionComponent implements OnInit {
       case '1':
         return 'Open';
       case '2':
-        return 'Meerkeuze';
+        return 'Multiple';
       case '3':
         return 'Program';
       default:
